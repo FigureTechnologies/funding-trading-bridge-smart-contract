26347590d492c21126b4794f6ed3aa6e2d9cf2c1
Jake Schwartz <jschwartz@figure.com>
2024-01-31 15:47:58 -0800

    Go to the transferring strategy

--- src/execute/fund_trading.rs
+++ src/execute/fund_trading.rs
@@ -1,23 +1,38 @@
 use crate::store::contract_state::{get_contract_state_v1, CONTRACT_TYPE};
 use crate::types::error::ContractError;
 use crate::util::conversion_utils::convert_denom;
-use crate::util::provenance_utils::check_account_has_all_attributes;
-use crate::util::validation_utils::get_single_coin_input;
-use cosmwasm_std::{BankMsg, DepsMut, Env, MessageInfo, Response, Uint128};
+use crate::util::provenance_utils::{
+    check_account_has_all_attributes, check_account_has_enough_denom,
+};
+use crate::util::validation_utils::check_funds_are_empty;
+use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
 use provwasm_std::types::cosmos::base::v1beta1::Coin;
-use provwasm_std::types::provenance::marker::v1::{MsgMintRequest, MsgWithdrawRequest};
+use provwasm_std::types::provenance::marker::v1::{
+    MsgMintRequest, MsgTransferRequest, MsgWithdrawRequest,
+};
 use result_extensions::ResultExtensions;
 
-pub fn fund_trading(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, ContractError> {
+pub fn fund_trading(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    trade_amount: u128,
+) -> Result<Response, ContractError> {
+    check_funds_are_empty(&info)?;
     let contract_state = get_contract_state_v1(deps.storage)?;
-    let input_coin = get_single_coin_input(&info, &contract_state.deposit_marker.name)?;
+    check_account_has_enough_denom(
+        &deps,
+        info.sender.as_str(),
+        &contract_state.deposit_marker.name,
+        trade_amount,
+    )?;
     check_account_has_all_attributes(
         &deps,
         &info.sender,
         &contract_state.required_deposit_attributes,
     )?;
     let conversion = convert_denom(
-        input_coin.amount.u128(),
+        trade_amount,
         &contract_state.deposit_marker,
         &contract_state.trading_marker,
     )?;
@@ -25,13 +40,24 @@ pub fn fund_trading(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Respon
         return ContractError::InvalidFundsError {
             message: format!(
                 "sent [{}{}], but that is not enough to convert to at least one [{}]",
-                input_coin.amount.u128(),
+                trade_amount,
                 &contract_state.deposit_marker.name,
                 &contract_state.trading_marker.name,
             ),
         }
         .to_err();
     }
+    // Transfer the necessary amount from the sender (total amount requested - remainder that cannot be converted)
+    let transferred_amount = trade_amount - conversion.remainder;
+    let transfer_msg = MsgTransferRequest {
+        administrator: env.contract.address.to_string(),
+        amount: Some(Coin {
+            denom: contract_state.deposit_marker.name.to_owned(),
+            amount: transferred_amount.to_string(),
+        }),
+        from_address: info.sender.to_string(),
+        to_address: env.contract.address.to_string(),
+    };
     // Mint the amount of coin to which the conversion equates
     let minted_coin = Coin {
         denom: contract_state.trading_marker.name.to_owned(),
@@ -48,19 +74,8 @@ pub fn fund_trading(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Respon
         to_address: info.sender.to_string(),
         amount: vec![minted_coin.to_owned()],
     };
-    // If the sender specified more coin than can be converted, send back the remainder
-    let refund_msg = if conversion.remainder > 0 {
-        Some(BankMsg::Send {
-            amount: vec![cosmwasm_std::Coin {
-                amount: Uint128::new(conversion.remainder),
-                denom: contract_state.deposit_marker.name.to_owned(),
-            }],
-            to_address: info.sender.to_string(),
-        })
-    } else {
-        None
-    };
     let mut response = Response::new()
+        .add_message(transfer_msg)
         .add_message(mint_msg)
         .add_message(withdraw_msg)
         .add_attribute("action", "fund_trading")
@@ -68,21 +83,9 @@ pub fn fund_trading(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Respon
         .add_attribute("contract_type", CONTRACT_TYPE)
         .add_attribute("contract_name", &contract_state.contract_name)
         .add_attribute("deposit_input_denom", &contract_state.deposit_marker.name)
-        .add_attribute(
-            "deposit_requested_amount",
-            input_coin.amount.u128().to_string(),
-        )
-        .add_attribute(
-            "deposit_actual_amount",
-            conversion.target_amount.to_string(),
-        )
+        .add_attribute("deposit_requested_amount", trade_amount.to_string())
+        .add_attribute("deposit_actual_amount", transferred_amount.to_string())
         .add_attribute("received_denom", minted_coin.denom)
         .add_attribute("received_amount", minted_coin.amount);
-    if let Some(refund) = refund_msg {
-        response = response
-            .add_message(refund)
-            .add_attribute("refund_denom", &contract_state.deposit_marker.name)
-            .add_attribute("refund_amount", conversion.remainder.to_string())
-    }
     response.to_ok()
 }
--- src/execute/withdraw_trading.rs
+++ src/execute/withdraw_trading.rs
@@ -1,27 +1,36 @@
 use crate::store::contract_state::{get_contract_state_v1, CONTRACT_TYPE};
 use crate::types::error::ContractError;
 use crate::util::conversion_utils::convert_denom;
-use crate::util::provenance_utils::check_account_has_all_attributes;
-use crate::util::validation_utils::get_single_coin_input;
-use cosmwasm_std::{BankMsg, DepsMut, Env, MessageInfo, Response, Uint128};
+use crate::util::provenance_utils::{
+    check_account_has_all_attributes, check_account_has_enough_denom, get_marker_address_for_denom,
+};
+use crate::util::validation_utils::check_funds_are_empty;
+use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
 use provwasm_std::types::cosmos::base::v1beta1::Coin;
-use provwasm_std::types::provenance::marker::v1::MsgBurnRequest;
+use provwasm_std::types::provenance::marker::v1::{MsgBurnRequest, MsgTransferRequest};
 use result_extensions::ResultExtensions;
 
 pub fn withdraw_trading(
     deps: DepsMut,
     env: Env,
     info: MessageInfo,
+    trade_amount: u128,
 ) -> Result<Response, ContractError> {
+    check_funds_are_empty(&info)?;
     let contract_state = get_contract_state_v1(deps.storage)?;
-    let input_coin = get_single_coin_input(&info, &contract_state.trading_marker.name)?;
+    check_account_has_enough_denom(
+        &deps,
+        info.sender.as_str(),
+        &contract_state.trading_marker.name,
+        trade_amount,
+    )?;
     check_account_has_all_attributes(
         &deps,
         &info.sender,
         &contract_state.required_withdraw_attributes,
     )?;
     let conversion = convert_denom(
-        input_coin.amount.u128(),
+        trade_amount,
         &contract_state.trading_marker,
         &contract_state.deposit_marker,
     )?;
@@ -29,44 +38,46 @@ pub fn withdraw_trading(
         return ContractError::InvalidFundsError {
             message: format!(
                 "sent [{}{}], but that is not enough to convert to at least one [{}]",
-                input_coin.amount.u128(),
+                trade_amount,
                 &contract_state.trading_marker.name,
                 &contract_state.deposit_marker.name,
             ),
         }
         .to_err();
     }
+    let collected_amount = trade_amount - conversion.remainder;
+    // Collect the amount to be traded to the contract from the sender and give it directly to the
+    // marker in order to stage it for burning
+    let collect_funds_msg = MsgTransferRequest {
+        administrator: env.contract.address.to_string(),
+        amount: Some(Coin {
+            denom: contract_state.trading_marker.name.to_owned(),
+            amount: collected_amount.to_string(),
+        }),
+        from_address: info.sender.to_string(),
+        to_address: get_marker_address_for_denom(&deps, &contract_state.trading_marker.name)?,
+    };
     // Release the total converted amount of funds back to the user
-    let release_funds_msg = BankMsg::Send {
-        amount: vec![cosmwasm_std::Coin {
-            amount: Uint128::new(conversion.target_amount),
+    let release_funds_msg = MsgTransferRequest {
+        administrator: env.contract.address.to_string(),
+        amount: Some(Coin {
             denom: contract_state.deposit_marker.name.to_owned(),
-        }],
+            amount: conversion.target_amount.to_string(),
+        }),
+        from_address: env.contract.address.to_string(),
         to_address: info.sender.to_string(),
     };
     // Burn all coins that were received except those that could not be converted, these will be
     // refunded
-    let burn_amount = input_coin.amount.u128() - conversion.remainder;
     let burn_msg = MsgBurnRequest {
         administrator: env.contract.address.to_string(),
         amount: Some(Coin {
-            amount: burn_amount.to_string(),
+            amount: collected_amount.to_string(),
             denom: contract_state.trading_marker.name.to_owned(),
         }),
     };
-    // Refund any amount of input coins that could not be successfully converted
-    let refund_msg = if conversion.remainder > 0 {
-        Some(BankMsg::Send {
-            amount: vec![cosmwasm_std::Coin {
-                amount: Uint128::new(conversion.remainder),
-                denom: contract_state.trading_marker.name.to_owned(),
-            }],
-            to_address: info.sender.to_string(),
-        })
-    } else {
-        None
-    };
-    let mut response = Response::new()
+    Response::new()
+        .add_message(collect_funds_msg)
         .add_message(release_funds_msg)
         .add_message(burn_msg)
         .add_attribute("action", "withdraw_trading")
@@ -74,17 +85,9 @@ pub fn withdraw_trading(
         .add_attribute("contract_type", CONTRACT_TYPE)
         .add_attribute("contract_name", &contract_state.contract_name)
         .add_attribute("withdraw_input_denom", &contract_state.trading_marker.name)
-        .add_attribute(
-            "withdraw_input_amount",
-            input_coin.amount.u128().to_string(),
-        )
+        .add_attribute("withdraw_input_amount", trade_amount.to_string())
+        .add_attribute("withdraw_actual_amount", collected_amount.to_string())
         .add_attribute("received_denom", &contract_state.deposit_marker.name)
-        .add_attribute("received_amount", conversion.target_amount.to_string());
-    if let Some(refund) = refund_msg {
-        response = response
-            .add_message(refund)
-            .add_attribute("refund_denom", &contract_state.trading_marker.name)
-            .add_attribute("refund_amount", conversion.remainder.to_string())
-    }
-    response.to_ok()
+        .add_attribute("received_amount", conversion.target_amount.to_string())
+        .to_ok()
 }
